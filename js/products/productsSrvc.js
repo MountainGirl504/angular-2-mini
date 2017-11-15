aa288bb3d4efb0817325fc343470280d55a800e6
MountainGirl504 <natalia_calt@yahoo.com>
2017-11-15 14:00:20 -0700

    complete

--- js/products/productsSrvc.js
+++ js/products/productsSrvc.js
@@ -1,3 +1,18 @@
-angular.module('myApp').service('productsSrvc', function() {
+angular.module('myApp').service('productsSrvc', function($http) {
+    
+    this.getShoeData = function (){
+        return $http.get('https://practiceapi.devmountain.com/products?category=shoes')
+        
+        .then( resp => {
+            console.log("Response:", resp)
+            return resp.data;
+        })
+    }
 
+        this.getSockData = function (){
+            return $http.get('https://practiceapi.devmountain.com/products?category=socks')
+            .then(resp => {
+                return resp.data;
+            })
+        }
 });
